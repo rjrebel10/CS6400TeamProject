8bbbfc970c0f00c6977c5ae353c97dca9341f5fc
rjrebel10 <31456118+rjrebel10@users.noreply.github.com>
2018-05-29 16:33:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+from socket import gethostname
 import os
  
+# App config.
+DEBUG = True
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
  
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -37,5 +42,5 @@ def logout():
     return render_template('loggedout.html')
 
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
-    app.run(debug=True)
+    if 'liveconsole' not in gethostname():
+        app.run()
